5b01a29b4cf6d23ca433389bac07479e2ea1ec28
Forever2Me <liubokai0213@gmail.com>
2022-03-08 17:01:05 +0800

    2022年3月8日 下午改

--- 05_02字典的其他使用.py
+++ 05_02字典的其他使用.py
@@ -1,13 +1,22 @@
 xiaoming_dict = {"name": "小明",
-                 "age": "16"}
+                 "age": 16,
+                 "size":111}
 
 # 1.统计键值对数量
 print(len(xiaoming_dict))
 
 # 2.合并字典
 temp_dict = {"height": 1.75}
-
+# 如果被合并的字典存在已有的键值对，会覆盖原有的键值对
 
 xiaoming_dict.update(temp_dict)
+
+# 3、清空字典
+# xiaoming_dict.clear()
+
+# 4、删除指定键值对
+xiaoming_dict.pop("age")
+
+# 5.删除随机一个键值对
+xiaoming_dict.popitem()
 print(xiaoming_dict)
-print(xiaoming_dict["age"])
